7ea5cdc3eea92eb832851d6d2abe6b4a001bf515
Subhadeep <71310704+PHPxCODER@users.noreply.github.com>
2025-07-04 11:37:59 +0530

    Vercel Analytics 👀

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/next"
 import { Inter, Dancing_Script, Playfair_Display, Poppins } from "next/font/google";
 import { ThemeProvider } from "@/components/theme-provider"
 import "./globals.css";
@@ -163,6 +164,7 @@ export default function RootLayout({
           >
             {children}
           </ThemeProvider>
+        <Analytics />
       </body>
     </html>
   );
