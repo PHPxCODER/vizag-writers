98aa23ca31d00797841cb40c09b243ff1cf83fe4
Subhadeep Pramanik <22051558@kiit.ac.in>
2025-11-03 09:48:28 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import { ComingSoonPage } from "@/components/coming-soon/ComingSoonPage";
 export default function Home() {
   return (
     <ComingSoonPage
-      launchDate={new Date("2025-11-20T11:00:00+05:30")} // Adjust your launch date
+      launchDate={new Date("2025-11-16T11:00:00+05:30")} // Adjust your launch date
       title="Vizag Writers: A New Chapter is Coming Soon!"
       subtitle="The heart of Vizag's writing community is evolving. Get ready for an even more vibrant space where every voice finds its stage, every story matters, and every writer truly belongs. Join us as we unveil our new home!"
     />
